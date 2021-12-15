0a3f768f9e8ed642d63b65da6b7a9024e9a4a847
Kuldeep Singh Bhandari <singh.kuldeepbhandari@gmail.com>
2021-12-15 19:43:49 +0530

    02 finished

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+  
+  return <article className="single-tour">
+    <img src={image} alt={name} />
+    <footer>
+      <div className="tour-info">
+        <h4>{name}</h4>
+        <h4 className="tour-price">${price}</h4>
+      </div>
+      {readMore && <p>{info}</p>}
+      {readMore && <button onClick={() => setReadMore(false)}>read less</button>}
+      {!readMore && <p>{info.substring(0, 100)}</p>}
+      {!readMore && <button onClick={() => setReadMore(true)}>read more</button>}
+      <button className="delete-btn" onClick={() => removeTour(id)}>not interested</button>
+    </footer>
+  </article>;
 };
 
 export default Tour;
