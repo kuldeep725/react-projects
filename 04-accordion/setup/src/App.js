bc1ac05c5cabfaa55406a9cc2547d53a3c774979
Kuldeep Singh Bhandari <singh.kuldeepbhandari@gmail.com>
2021-12-16 22:03:30 +0530

    04 finished

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import Question from "./Question";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <div>
+          <h3>questions and answers about login</h3>
+        </div>
+        <div>
+          {data.map((question) => (
+            <Question {...question} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
